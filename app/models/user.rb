39c4b373802c23895791349a656bb869a597f925
Gabriel <gabarros17@gmail.com>
2018-08-19 13:12:18 -0300

    Edited User.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,29 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tweets
- 
 
-  has_many :followers, class_name: "Relationship", foreign_key: "follower_id"
-  has_many :following, class_name: "Relationship", foreign_key: "followed_id"
+  has_many :active_relationships, class_name: 'Relationship',
+  		    foreign_key: 'follower_id'. dependent: :destroy
+
+  has_many :passive_relationships, class_name: 'Relationship',
+ 			foreign_key: 'followed_id', dependent: :destroy
+
+ has_many :following, through: :active_relationships, source: :followed 
+ has_many :followers, through: :passive_relationships, source: :follower
+
+ def following?(other_user)
+
+ 	
+ end
+
+ def follow!(other_user)
+ 	
+ end
+
+ def unfollow
+ 	
+ end
+
+
 
 end
